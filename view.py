80904fc2dd916089f1b83577a4abbe10e32989c0
Mixail <mixail.korchikov@mail.ru>
2022-02-09 18:10:34 +0300

    minor

--- view.py
+++ view.py
@@ -2,7 +2,14 @@ from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 
 app = FastAPI()
-@app.get('/view')
-async def view():
-    with open('view.html', 'r') as file:
+@app.get('/')
+async def view(model: int):
+    model_name = ''
+
+    if model == 1:
+        model_name = 'view1.html'
+    else:
+        model_name = 'view2.html'
+
+    with open(model_name, 'r') as file:
         return HTMLResponse(file.read())
